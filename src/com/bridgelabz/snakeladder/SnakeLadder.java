9d1d77001cc43ac1b95aa21d2ecb41cf24f6c1a0
hp-pc <navyashree811@gmail.com>
2022-08-05 00:32:14 +0530

    [Navaya]Added : Roll the die to get number between 1 to 6

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -1,17 +1,33 @@
 /*
- * Snake and Ladder game playe with single player at start position 0 .
- */
+* UC2 : Snake and Ladder game play with single player at start position 0 .
+*       The player roll the die and get the number between 1 to 6 .
+*/
 package com.bridgelabz.snakeladder;
 
+import java.util.Scanner;
+
 public class SnakeLadder {
+	// use method to roll die
+	static void rolldie() {
+		int min = 1;
+		int max = 6;
+		int Number = (int) Math.floor(Math.random() * (max - min) + min);
+
+		System.out.println("Die = " + Number);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Simulator problems ");
-		int player = 1;
 
 		int position = 0;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter how many players want to play");
+		int player = sc.nextInt();
 
 		System.out.println("Player in the Position = " + position);
 
+		// Function call
+		rolldie();
 	}
 
 }
