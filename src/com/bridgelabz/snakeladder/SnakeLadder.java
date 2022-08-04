b71b2065986b9af6db784273ebd98d251ffb70dd
hp-pc <navyashree811@gmail.com>
2022-08-05 00:21:01 +0530

    [Navaya]Added : Game played with single player

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -1,8 +1,17 @@
+/*
+ * Snake and Ladder game playe with single player at start position 0 .
+ */
 package com.bridgelabz.snakeladder;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Simulator problems ");
+		int player = 1;
+
+		int position = 0;
+
+		System.out.println("Player in the Position = " + position);
+
 	}
 
 }
