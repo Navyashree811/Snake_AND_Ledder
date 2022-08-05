6e4d5a2929e362e29422db1b19a05e312e5a470d
hp-pc <navyashree811@gmail.com>
2022-08-05 08:42:09 +0530

    [Navya]Added : Check option they are no play, Ladder or Snake

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -1,33 +1,47 @@
 /*
-* UC2 : Snake and Ladder game play with single player at start position 0 .
-*       The player roll the die and get the number between 1 to 6 .
+* UC3 : The Player then checks for a option. They are No Play, Ladder or Snake.
 */
 package com.bridgelabz.snakeladder;
 
-import java.util.Scanner;
-
 public class SnakeLadder {
 	// use method to roll die
+
 	static void rolldie() {
 		int min = 1;
 		int max = 6;
-		int Number = (int) Math.floor(Math.random() * (max - min) + min);
+		int dieNumber = (int) Math.floor(Math.random() * (max - min) + min);
 
-		System.out.println("Die = " + Number);
+		System.out.println("Die = " + dieNumber);
+	}
+
+	static void checkPlayer(int check) {
+		// check they are no play, ladder or snake
+		switch (check) {
+		case 0:
+			System.out.println("They are no play");
+			break;
+		case 1:
+			System.out.println("Ladder");
+			rolldie();
+			break;
+		case 2:
+			System.out.println("Snake");
+			rolldie();
+			break;
+		default:
+			System.out.println("Some thing went wrong");
+		}
 	}
 
 	public static void main(String[] args) {
-
 		int position = 0;
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter how many players want to play");
-		int player = sc.nextInt();
-
+		int player = 1;
 		System.out.println("Player in the Position = " + position);
 
-		// Function call
-		rolldie();
-	}
+		// Generate random number
+		int check = (int) Math.floor((Math.random() * 10) % 3);
 
+		// static method call
+		checkPlayer(check);
+	}
 }
